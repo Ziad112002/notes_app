6c5c5a545975d5fd23cf31a6af938e913c535879
ziad <mqpz596@gmail.com>
2024-07-14 16:04:23 +0300

    change add note loading

--- lib/simple_bloc_observer.dart
+++ lib/simple_bloc_observer.dart
@@ -4,17 +4,17 @@ import 'package:flutter/material.dart';
 class SimpleBlocObserver implements BlocObserver {
   @override
   void onChange(BlocBase bloc, Change change) {
-    debugPrint('change=$change');
+    debugPrint('change = $change');
   }
 
   @override
   void onClose(BlocBase bloc) {
-    debugPrint('closed=$bloc');
+    debugPrint('close = $bloc');
   }
 
   @override
   void onCreate(BlocBase bloc) {
-    debugPrint('create=$bloc');
+    debugPrint('create = $bloc');
   }
 
   @override
--- lib/widgets/custom_button.dart
+++ lib/widgets/custom_button.dart
@@ -2,8 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:notes_app/constants.dart';
 
 class CustomButton extends StatelessWidget {
-  const CustomButton({super.key, this.onTap});
+  const CustomButton({super.key, this.onTap, this.isLoading = false});
   final void Function()? onTap;
+  final bool isLoading;
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
@@ -13,12 +14,18 @@ class CustomButton extends StatelessWidget {
         height: 55,
         decoration: BoxDecoration(
             color: kPrimaryColor, borderRadius: BorderRadius.circular(8)),
-        child: const Center(
-          child: Text(
-            'Add',
-            style: TextStyle(
-                fontSize: 20, color: Colors.black, fontWeight: FontWeight.bold),
-          ),
+        child: Center(
+          child: isLoading
+              ? const CircularProgressIndicator(
+                  color: Colors.black,
+                )
+              : const Text(
+                  'Add',
+                  style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold),
+                ),
         ),
       ),
     );
