239e1f7873d67b900debba999eabc46df3b8023d
ziad <mqpz596@gmail.com>
2024-07-17 15:55:48 +0300

    add icon to the app

--- lib/constants.dart
+++ lib/constants.dart
@@ -3,11 +3,14 @@ import 'package:flutter/material.dart';
 const kPrimaryColor = Color(0xff62fcd7);
 const kNotesBox = 'notes_box';
 List<Color> kColors = const [
-  Color(0xffd5bca2),
-  Color(0xff55beaf),
-  Color(0xffa5bca2),
-  kPrimaryColor,
-  Color.fromARGB(255, 201, 172, 167),
+  Color(0xffDEF9C4),
+  Color(0xff9CDBA6),
+  Color(0xff50B498),
+  Color(0xff468585),
   Color(0xff4859bd),
+  Color(0xffDCA47C),
+  Color(0xffFFD3B6),
+  Color(0xffFCF8F3),
+  Color(0xff698474),
   Color(0xffdf4f4d),
 ];
